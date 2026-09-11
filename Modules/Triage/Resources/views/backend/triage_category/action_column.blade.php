fbd54d8157b5a078aa71a5ffc42dcdea954955c4
zakria tunio <muhammadzakriatunio@gmail.com>
2026-09-11 16:39:27 +0500

    Refactor action buttons in triage category view

--- Modules/Triage/Resources/views/backend/triage_category/action_column.blade.php
+++ Modules/Triage/Resources/views/backend/triage_category/action_column.blade.php
@@ -1,24 +1,34 @@
-<div class="d-flex gap-2 align-items-center">
+<div class="d-flex align-items-center gap-3">
     @hasPermission('edit_triage_category')
-    <button type="button" class="btn btn-soft-primary btn-sm"
+        <button
+            type="button"
+            class="btn text-success p-0 fs-5"
             onclick="editCategory({{ $data->id }})"
             data-bs-toggle="tooltip"
-            title="{{ __('messages.edit') }}">
-        <i class="fa-solid fa-pen-clip"></i>
-    </button>
+            title="{{ __('messages.edit') }}"
+            aria-label="{{ __('messages.edit') }}"
+        >
+            <i class="ph ph-pencil-simple-line align-middle"></i>
+        </button>
     @endhasPermission
 
     @hasPermission('delete_triage_category')
-    <a href="{{ route('backend.triage-category.destroy', $data->id) }}"
-       id="delete-{{ $module_name }}-{{ $data->id }}"
-       class="btn btn-soft-danger btn-sm"
-       data-type="ajax"
-       data-method="DELETE"
-       data-token="{{ csrf_token() }}"
-       data-bs-toggle="tooltip"
-       title="{{ __('messages.delete') }}"
-       data-confirm-yes="{{ __('messages.action_warning_message') }}">
-        <i class="fa-solid fa-trash-can"></i>
-    </a>
+        <a
+            href="{{ route('backend.triage-category.destroy', $data->id) }}"
+            id="delete-{{ $module_name }}-{{ $data->id }}"
+            class="btn text-danger p-0 fs-5"
+            data-type="ajax"
+            data-method="DELETE"
+            data-token="{{ csrf_token() }}"
+            data-bs-toggle="tooltip"
+            title="{{ __('messages.delete') }}"
+            aria-label="{{ __('messages.delete') }}"
+            data-confirm="{{ __('messages.are_you_sure?', [
+                'form' => $data->name ?? __('Unknown'),
+                'module' => __('triage.category'),
+            ]) }}"
+        >
+            <i class="ph ph-trash align-middle"></i>
+        </a>
     @endhasPermission
 </div>
